65296f95d76137426ce659d1312436f9e8e6fb4c
std51 <Bitstd51@gmail.com>
2024-05-30 12:46:02 +0545

    inheritance in php

---
+++ accessModifiers.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    /*
+    //Private , protected and public
+    class Car{
+        public $name;
+        private $color;
+        protected $model;
+    
+    }
+
+    $volvo = new Car();
+   echo $volvo->name = 'Volvo';
+    echo    $volvo->color = 'gray'; // throws an error
+    echo $volvo->model = 'bycml'; // throws an error
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+*/
+
+
+    // Inheritance in PHP
+    // Extends in Inheritance??
+
+/*
+
+    class Fruit {
+      public $name;
+      public $color;
+      public function __construct($name, $color) {
+        $this->name = $name;
+        $this->color = $color;
+      }
+      public function intro() {
+        echo "The fruit is {$this->name} and the color is {$this->color}.";
+      }
+    }
+    
+    // Strawberry is inherited from Fruit
+    class Strawberry extends Fruit {
+      public function message() {
+        echo "Am I a fruit or a berry? ";
+      }
+    }
+    $strawberry = new Strawberry("Strawberry", "red");
+    $strawberry->message();
+    $strawberry->intro();
+    
+*/
+
+
+// Interfaces in PHP = provides abstraction 
+// Abstract class = provides abstraction and implementation
+// Traits = provides multiple inheritance
+// Inheritance = provides implementation
+
+
+
+    
+
+
+    
+
+    ?>
+</body>
+</html>
