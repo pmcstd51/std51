d3a80ce3c567f906ae929510a563539902fe216c
std51 <Bitstd51@gmail.com>
2024-07-04 12:28:11 +0545

    crud added but its incomplete

---
+++ CRUD.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include "config.php";
+    if(isset($_POST['submit'])){
+        $id = $_POST['id'];
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $gender = $_POST['gender'];
+        $sql = "INSERT INTO ``"
+
+    }
+    ?>
+</body>
+</html>
